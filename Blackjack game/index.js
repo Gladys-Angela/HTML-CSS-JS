0d6a7442b4514f36af84ab5291b6c2873c1fcdc7
Gladys-Angela <gladysangelakamau@gmail.com>
2025-02-07 19:22:31 +0300

    newcardbtn

--- Blackjack game/index.js
+++ Blackjack game/index.js
@@ -6,9 +6,18 @@
 // less or equal to 20 = Do you want to draw a new card😊
 //21 = You got blackjack 🥳
 //else = You're out of the game😥
+//store the message-el paragraph in a variable called messageEl
+//display the message in the messageEl using messageEl.textContent
+//store the sum paragraph in a variable called sumEl
+//Render the sum on the page using this format = "Sum: 14"
+//store the cards paragraph in a variable called cardsEl
+//Render the cars on the page using this format = "Cards: 10 4"
+let cardsEl = document.getElementById("cards-el")
+let sumEl = document.getElementById("sum-el")
+let messageEl = document.getElementById("message-el")
 let firstCard = 10
 let secondCard = 10
-let sum = firstCard + secondCard + 5
+let sum = firstCard + secondCard 
 let hasBlackJack = false
 let isAlive = true
 let message = ""
@@ -16,15 +25,27 @@ let message = ""
 function startGame() {
     if (sum <= 20) {
         message = "Do you want to draw a new card😊"
+        
     } else if (sum === 21) {
         message ="You got blackjack 🥳"
         hasBlackJack = true
+        
     } else{
         message ="You're out of the game😥"
         isAlive = false
+        
     }
 
     //console.log(hasBlackJack)
     //console.log(isAlive)
-    console.log(message)
+    //console.log(message)
+    messageEl.textContent = message
+    sumEl.textContent = "Sum: " + sum
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
 }
+ //create a function newCard() that logs out "Drawing a new card"
+
+ function newCard(){
+    console.log("Drawing a new card")
+ }
+  
