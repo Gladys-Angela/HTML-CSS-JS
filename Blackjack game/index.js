9b6cae64e016307fb1b414d5891d86c1498698c3
Gladys-Angela <gladysangelakamau@gmail.com>
2025-02-07 17:39:21 +0300

    if..if.else statement

--- Blackjack game/index.js
+++ Blackjack game/index.js
@@ -9,15 +9,20 @@ let firstCard = 10
 let secondCard = 7
 let hasBlackJack = false
 let sum = firstCard + secondCard
+let isAlive = true
+let message = ""
 console.log(sum)
 
 if (sum <= 20) {
-    console.log("Do you want to draw a new card😊")
+    message = "Do you want to draw a new card😊"
 } else if (sum == 21) {
-    console.log("You got blackjack 🥳")
+    message ="You got blackjack 🥳"
     hasBlackJack = true
 } else{
-    console.log("You're out of the game😥")
+    message ="You're out of the game😥"
+    isAlive = false
 }
 //CASH OUT
 console.log(hasBlackJack)
+console.log(isAlive)
+console.log(message)
