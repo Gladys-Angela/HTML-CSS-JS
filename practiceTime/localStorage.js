72c86afa71b56045547af0fff40bb9c417b6cc99
Gladys-Angela <gladysangelakamau@gmail.com>
2025-04-17 19:41:08 +0300

    localStorage

---
+++ practiceTime/localStorage.js
@@ -0,0 +1,7 @@
+//save a value to localstorage
+//Delete you code and refresh the page
+//fetch your value from localstorage and log it out
+//localStorage.setItem("myCredits, "100")
+let myCredits = localStorage.getItem("myCredits")
+console.log(myCredits)
+
